c4e626dd5bbaf288ce69817fb6eacc744c117290
h586616 <lotte-m97@hotmail.com>
2021-04-19 18:58:51 +0200

    computelogic

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
@@ -42,7 +42,22 @@ public class Util {
 		// it modifies 'upper' and 'id' when lower > upper e.g. set (6, 2) in mod 10 = {6, 7, 8, 9, 0, 1, 2}
 		
 		// implement: read the descriptions above
+		
 		boolean cond = false;
+		BigInteger up = upper;
+		BigInteger addressize = Hash.addressSize();
+		if(lower.compareTo(upper)==1) {
+			up = upper.add(addressize);
+			
+			if(id.compareTo(new BigInteger("0")) == 1) || id.compareTo(new BigInteger("0")) ==0) &&
+				(id.compareTo(upper) ==-1 || id.compareTo(upper) ==0)) {
+					id = id.add(addressize);
+				}
+		}
+		
+		upper = up;
+		cond = (id.compareTo(lower) ==1 || id.compareTo(lower) ==0) && (id.compareTo(upper) ==-1 || id.compareTo(upper) ==0);
+		
 
 		
 		return cond;
